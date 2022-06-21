8743b2dd64177c173f98a9bc0b12515e14dd5186
soupday <79094830+soupday@users.noreply.github.com>
2022-06-21 20:17:47 +0100

    1.3.3
    Version Number change.

--- Editor/Pipeline.cs
+++ Editor/Pipeline.cs
@@ -40,7 +40,7 @@ namespace Reallusion.Import
 
     public static class Pipeline
     {
-        public const string VERSION = "1.3.2";
+        public const string VERSION = "1.3.3";
 
 #if HDRP_10_5_0_OR_NEWER
         // version
