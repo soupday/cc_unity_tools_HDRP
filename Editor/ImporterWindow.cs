3b60bb7dc5ce6e47446a0c9b1bfbb1154efbd3a9
soupday <79094830+soupday@users.noreply.github.com>
2022-12-01 04:58:33 +0000

    1.4.1
    -

--- Editor/ImporterWindow.cs
+++ Editor/ImporterWindow.cs
@@ -186,7 +186,7 @@ namespace Reallusion.Import
         {                        
             Type hwt = Type.GetType("UnityEditor.SceneHierarchyWindow, UnityEditor.dll");
             ImporterWindow window = GetWindow<ImporterWindow>(windowTitle, hwt);
-            window.minSize = new Vector2(300f, 500f);
+            window.minSize = new Vector2(300f, 500f);            
             Current = window;
 
             ClearAllData();
@@ -718,9 +718,9 @@ namespace Reallusion.Import
                     restoreHairAfterGUI = true;
                 }
             }
-            else if (!contextCharacter.BuiltBasicMaterials && contextCharacter.HasColorEnabledHair())
+            else //if (!contextCharacter.BuiltBasicMaterials && contextCharacter.HasColorEnabledHair())
             {
-                //if (contextCharacter.BuiltBasicMaterials || !contextCharacter.HasColorEnabledHair()) GUI.enabled = false;
+                if (contextCharacter.BuiltBasicMaterials || !contextCharacter.HasColorEnabledHair()) GUI.enabled = false;
 
                 if (GUILayout.Button(new GUIContent(iconActionBakeHair, "Bake hair materials, to preview the baked results of the 'Enable Color' in the hair materials."),
                     GUILayout.Width(ACTION_BUTTON_SIZE), GUILayout.Height(ACTION_BUTTON_SIZE)))
